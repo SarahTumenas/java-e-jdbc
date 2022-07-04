de09fa750cf18d65febf6b6d4d479119b9cc0f6e
Sarah Tumenas <sarah.marques@spread.com.br>
2022-07-04 10:30:03 -0300

    Criando o modelo Produto

---
+++ src/main/java/br/com/alura/lojavirtual/TestaInsercaoComProduto.java
@@ -0,0 +1,33 @@
+package br.com.alura.lojavirtual;
+
+import br.com.alura.lojavirtual.modelo.Produto;
+
+import java.sql.*;
+
+public class TestaInsercaoComProduto {
+    public static void main(String[] args) throws SQLException {
+        Produto comoda = new Produto("Cômoda", "Cômoda vertical");
+
+        try (Connection connection = new ConnectionFactory().recuperarConexao()) {
+            String sql = "INSERT INTO PRODUTO (NOME, DESCRICAO) VALUES (?, ?)";
+            try (PreparedStatement statement = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)) {
+                statement.setString(1, comoda.getNome());
+                statement.setString(2, comoda.getDescricao());
+
+                statement.execute();
+
+                try (ResultSet resultSet = statement.getGeneratedKeys()) {
+                    while (resultSet.next()) {
+                        comoda.setId(resultSet.getInt(1));
+                    }
+
+                }
+            }
+
+        }
+        System.out.println(comoda);
+
+        }
+
+    }
+
